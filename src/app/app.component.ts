ee1f6954a74c9a8ce51bb8252be370142e357d71
NandoProgram <luisfernandolopezmorenoprogram@gmail.com>
2025-05-02 18:11:57 -0600

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Practica_10';
+
+  isvisible:boolean=true;
+  toggle():void{
+    this.isvisible=!this.isvisible;
+  }
+
+  /*-----------------------------*/
+
+  Lenguajes:string[]=['C++','Java','Phyton'];
+  nuevo:string='';
+  
+  addItem():void{
+    this.Lenguajes.push(this.nuevo);
+    this.nuevo='';
+  }
+
+  selectedOption:string='deafault';
+
 }
